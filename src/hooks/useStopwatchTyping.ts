54b2a93d9c377072467c324392dfccc2e95ef579
Fedor4994 <103943126+Fedor4994@users.noreply.github.com>
2023-02-21 12:31:53 +0200

    Words page refactoring

---
+++ src/hooks/useStopwatchTyping.ts
@@ -0,0 +1,85 @@
+import { useState, useRef, useEffect, useCallback } from "react";
+import useStopwatch from "./useStopwatch";
+import { State } from "./useTimerTyping";
+import useWords from "./useWords";
+import { countErrors, isKeyboardAllowed } from "../utils/helpers";
+
+export const useStopwatchTyping = () => {
+  const [typed, setTyped] = useState("");
+  const [totalTyped, setTotalTyped] = useState(0);
+  const [errors, setErrors] = useState(0);
+  const [state, setState] = useState<State>("start");
+
+  const totalTypedRef = useRef(0);
+  const { words, updateWords, wordsCount, setWordsCount } = useWords();
+  const { secondsPassed, startStopwatch, resetStopwatch } = useStopwatch();
+
+  useEffect(() => {
+    // Finish words
+    if (typed.length === words.length) {
+      setTotalTyped(totalTypedRef.current);
+      setErrors(countErrors(typed, words));
+      resetStopwatch();
+      setState("finish");
+    }
+  }, [resetStopwatch, typed, updateWords, words]);
+
+  const onRestart = useCallback(() => {
+    totalTypedRef.current = 0;
+
+    setTyped("");
+    updateWords();
+    setTotalTyped(0);
+    setErrors(0);
+    resetStopwatch();
+    setState("start");
+  }, [resetStopwatch, updateWords]);
+
+  const keydownHandler = useCallback(
+    ({ key, code }: KeyboardEvent) => {
+      if (key === "Escape") {
+        onRestart();
+        return;
+      }
+
+      const allowed = isKeyboardAllowed(code);
+      if (!allowed || state === "finish") {
+        return;
+      }
+      if (state === "start") {
+        setState("run");
+        startStopwatch();
+      }
+      switch (key) {
+        case "Backspace":
+          setTyped((typed) => typed.slice(0, -1));
+          totalTypedRef.current -= 1;
+          break;
+        default:
+          setTyped((typed) => typed.concat(key));
+          totalTypedRef.current += 1;
+          break;
+      }
+    },
+    [onRestart, startStopwatch, state]
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", keydownHandler);
+    return () => {
+      window.removeEventListener("keydown", keydownHandler);
+    };
+  }, [keydownHandler]);
+
+  return {
+    wordsCount,
+    setWordsCount,
+    onRestart,
+    words,
+    typed,
+    errors,
+    totalTyped,
+    secondsPassed,
+    state,
+  };
+};
--- src/views/WordsPage/WordsPage.tsx
+++ src/views/WordsPage/WordsPage.tsx
@@ -1,86 +1,27 @@
-import { useState, useRef, useEffect, useCallback } from "react";
 import GeneratedWords from "../../components/GeneratedWords/GeneratedWords";
 import Navigation from "../../components/Navigation/Navigation";
 import RestartButton from "../../components/RestartButton/RestartButton";
 import Results from "../../components/Results/Results";
 import UserTyping from "../../components/UserTyping/UserTyping";
-import useStopwatch from "../../hooks/useStopwatch";
-import { State } from "../../hooks/useTimerTyping";
-import useWords from "../../hooks/useWords";
+import { useStopwatchTyping } from "../../hooks/useStopwatchTyping";
 import {
   calculateAccurancyPercentage,
   calculateWordsPerMinute,
-  countErrors,
-  isKeyboardAllowed,
 } from "../../utils/helpers";
 import s from "./WordsPage.module.scss";
 
 const WordsPage = () => {
-  const [typed, setTyped] = useState("");
-  const [totalTyped, setTotalTyped] = useState(0);
-  const [errors, setErrors] = useState(0);
-  const [state, setState] = useState<State>("start");
-
-  const totalTypedRef = useRef(0);
-  const { words, updateWords, wordsCount, setWordsCount } = useWords();
-  const { secondsPassed, startStopwatch, resetStopwatch } = useStopwatch();
-
-  useEffect(() => {
-    // Finish words
-    if (typed.length === words.length) {
-      setTotalTyped(totalTypedRef.current);
-      setErrors(countErrors(typed, words));
-      resetStopwatch();
-      setState("finish");
-    }
-  }, [resetStopwatch, typed, updateWords, words]);
-
-  const onRestart = useCallback(() => {
-    totalTypedRef.current = 0;
-
-    setTyped("");
-    updateWords();
-    setTotalTyped(0);
-    setErrors(0);
-    resetStopwatch();
-    setState("start");
-  }, [resetStopwatch, updateWords]);
-
-  const keydownHandler = useCallback(
-    ({ key, code }: KeyboardEvent) => {
-      if (key === "Escape") {
-        onRestart();
-        return;
-      }
-
-      const allowed = isKeyboardAllowed(code);
-      if (!allowed || state === "finish") {
-        return;
-      }
-      if (state === "start") {
-        setState("run");
-        startStopwatch();
-      }
-      switch (key) {
-        case "Backspace":
-          setTyped((typed) => typed.slice(0, -1));
-          totalTypedRef.current -= 1;
-          break;
-        default:
-          setTyped((typed) => typed.concat(key));
-          totalTypedRef.current += 1;
-          break;
-      }
-    },
-    [onRestart, startStopwatch, state]
-  );
-
-  useEffect(() => {
-    window.addEventListener("keydown", keydownHandler);
-    return () => {
-      window.removeEventListener("keydown", keydownHandler);
-    };
-  }, [keydownHandler]);
+  const {
+    wordsCount,
+    setWordsCount,
+    onRestart,
+    words,
+    typed,
+    errors,
+    totalTyped,
+    secondsPassed,
+    state,
+  } = useStopwatchTyping();
 
   return (
     <div className={s.wordsPage}>
